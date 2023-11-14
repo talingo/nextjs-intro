ea23fccbe6442a59240e930050ee6ffd37097fbd
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-11-14 02:07:17 -0300

    commented lusitana imort on acme-logo.tsx

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { clsx } from 'clsx';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '../inter';
 
 interface Breadcrumb {
   label: string;
@@ -21,7 +21,7 @@ export default function Breadcrumbs({
             key={breadcrumb.href}
             aria-current={breadcrumb.active}
             className={clsx(
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
+              breadcrumb.active ? 'text-gray-900' : 'text-gray-500'
             )}
           >
             <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from './inter';
 import {
   AtSymbolIcon,
   KeyIcon,
